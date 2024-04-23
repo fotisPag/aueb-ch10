680eafcf41650f919bbb4d526847bbd1696a1b6a
fotisPag <fotis.pagonis01@gmail.com>
2024-04-23 22:09:14 +0300

    ArraySwapMin

---
+++ ArraySwapMin.java
@@ -0,0 +1,43 @@
+package gr.aueb.cf.ch10;
+
+/*
+* Ανταλλάσει αμοιβαία το 1ο με το ελάχιστο
+* στοιχείο ενός πίνακα με τη χρήση των arrayMin
+* και της swap από την κλάση ArraySwap
+*
+* @fotisPag
+* */
+
+
+public class ArraySwapMin {
+
+    public static void main(String[] args) {
+
+        int[] ages = {50, 44, 33, 25, 18};
+
+        ArraySwap.swap(ages, 0, arrayMin(ages));
+
+        for (int age : ages) {
+            System.out.print(age + " ");
+        }
+
+
+    }
+
+    /*
+    * Θέτουμε αρχικά ως minPosition τη θέση 0.
+    * */
+
+    public static int arrayMin(int[] array){
+
+        int minPosition = 0;
+
+        for (int i = 1; i < array.length; i++){
+            if( array[i] < array[minPosition] ) {
+                minPosition = i;
+            }
+        }
+        return minPosition;
+    }
+
+}
